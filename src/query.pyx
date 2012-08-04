18b2fb750d49e4190fb4435ec88ad1fc708d2752
Chris P <sahib@online.de>
2012-08-04 22:28:38 +0200

    Added license blocks.

--- src/query.pyx
+++ src/query.pyx
@@ -1,3 +1,22 @@
+#################################################################
+# This file is part of plyr
+# + a commnandline tool and library to download various sort of musicrelated metadata.
+# + Copyright (C) [2011-2012]  [Christopher Pahl]
+# + Hosted at: https://github.com/sahib/python-glyr
+#
+# plyr is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# plyr is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with plyr. If not, see <http://www.gnu.org/licenses/>.
+#################################################################
 cimport glyr as C
 
 from cpython cimport bool
