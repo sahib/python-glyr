2942ba9d812cc42841c2a1493a3c11da4a78bcc5
Chris P <sahib@online.de>
2016-11-12 15:46:14 +0100

    License: change to LGPLv3 from GPLv3 to match libglyrs new license

--- src/glyr.pyx
+++ src/glyr.pyx
@@ -1,20 +1,20 @@
 #################################################################
 # This file is part of plyr
 # + a commnandline tool and library to download various sort of musicrelated metadata.
-# + Copyright (C) [2011-2012]  [Christopher Pahl]
+# + Copyright (C) [2011-2016]  [Christopher Pahl]
 # + Hosted at: https://github.com/sahib/python-glyr
 #
 # plyr is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
+# it under the terms of the GNU Lesser General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
 # plyr is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
+# GNU Lesser General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
+# You should have received a copy of the GNU Lesser General Public License
 # along with plyr. If not, see <http://www.gnu.org/licenses/>.
 #################################################################
 from glyr cimport glyr_init
